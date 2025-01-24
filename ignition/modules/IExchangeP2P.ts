8881bbdb60c588fa78a38c989a00a97798fd5728
baahkusi <nanakbaahkusi@gmail.com>
2025-01-24 14:12:48 +0000

    ignition modules

--- ignition/modules/IExchangeP2P.ts
+++ ignition/modules/IExchangeP2P.ts
@@ -7,23 +7,63 @@ import {
   FacetCutAction,
   INIT_SIG,
 } from "../lib";
-import IExchange from "./IExchangeDiamond";
+import IExchangeDiamondModule from "./IExchangeDiamond";
 
 const IExchangeP2PModule = buildModule("IExchangeP2PModule", (m) => {
-  const iExchange = m.useModule(IExchange);
+  const { iExchangeP2P, cutProxy } = m.useModule(IExchangeDiamondModule);
 
   const cpiS = functionSigsSelectors("IExchangeP2PInit");
   const cpi = m.contract("IExchangeP2PInit");
   const cpiInit = { contract: cpi, selector: cpiS[INIT_SIG] };
 
+  const mS = functionSelectors("Merchant");
+  const mt = m.contract("Merchant");
+  const mC = [mt, FacetCutAction.Add, Object.values(mS)];
+
+  const oS = functionSelectors("Order");
+  const o = m.contract("Order");
+  const oC = [o, FacetCutAction.Add, Object.values(oS)];
+
+  const aS = functionSelectors("Appeal");
+  const a = m.contract("Appeal");
+  const aC = [a, FacetCutAction.Add, Object.values(aS)];
+
+  const emS = functionSelectors("ExchangeManager");
+  const em = m.contract("ExchangeManager");
+  const emC = [em, FacetCutAction.Add, Object.values(emS)];
+
+  const evS = functionSelectors("ExchangeView");
+  const ev = m.contract("ExchangeView");
+  const evC = [ev, FacetCutAction.Add, Object.values(evS)];
+
   m.call(
-    iExchange.cutProxy,
+    cutProxy,
     "diamondCut",
-    [[], cpiInit.contract, cpiInit.selector],
+    [[oC, aC, emC, evC, mC], cpiInit.contract, cpiInit.selector],
     { id: "IExchangeP2PDiamondCut" }
   );
 
-  return {};
+  const acMerchant = m.contractAt("Merchant", iExchangeP2P, {
+    id: "IExchangeP2PMerchant",
+  });
+
+  const acOrder = m.contractAt("Order", iExchangeP2P, {
+    id: "IExchangeP2POrder",
+  });
+
+  const acAppeal = m.contractAt("Appeal", iExchangeP2P, {
+    id: "IExchangeP2PAppeal",
+  });
+
+  const acExchangeManager = m.contractAt("ExchangeManager", iExchangeP2P, {
+    id: "IExchangeP2PExchangeManager",
+  });
+
+  const acExchangeView = m.contractAt("ExchangeView", iExchangeP2P, {
+    id: "IExchangeP2PExchangeView",
+  });
+
+  return { acMerchant, acOrder, acAppeal, acExchangeManager, acExchangeView };
 });
 
 export default IExchangeP2PModule;
--- ignition/modules/IExchangeP2PUpdate.ts
+++ ignition/modules/IExchangeP2PUpdate.ts
@@ -7,23 +7,66 @@ import {
   FacetCutAction,
   INIT_SIG,
 } from "../lib";
-import IExchange from "./IExchangeDiamond";
+import IExchangeDiamondModule from "./IExchangeDiamond";
 
-const IExchangeP2PUpdateModule = buildModule("IExchangeP2PUpdateModule", (m) => {
-  const iExchange = m.useModule(IExchange);
+const IExchangeP2PUpdateModule = buildModule(
+  "IExchangeP2PUpdateModule",
+  (m) => {
+    const { iExchangeP2P, cutProxy } = m.useModule(IExchangeDiamondModule);
 
-  const cpiS = functionSigsSelectors("IExchangeP2PUpdateInit");
-  const cpi = m.contract("IExchangeP2PUpdateInit");
-  const cpiInit = { contract: cpi, selector: cpiS[INIT_SIG] };
+    const cpiS = functionSigsSelectors("IExchangeP2PInit");
+    const cpi = m.contract("IExchangeP2PInit");
+    const cpiInit = { contract: cpi, selector: cpiS[INIT_SIG] };
 
-  m.call(
-    iExchange.cutProxy,
-    "diamondCut",
-    [[], cpiInit.contract, cpiInit.selector],
-    { id: "IExchangeP2PUpdateDiamondCut" }
-  );
+    const mS = functionSelectors("Merchant");
+    const mt = m.contract("Merchant");
+    const mC = [mt, FacetCutAction.Replace, Object.values(mS)];
 
-  return {};
-});
+    const oS = functionSelectors("Order");
+    const o = m.contract("Order");
+    const oC = [o, FacetCutAction.Replace, Object.values(oS)];
+
+    const aS = functionSelectors("Appeal");
+    const a = m.contract("Appeal");
+    const aC = [a, FacetCutAction.Replace, Object.values(aS)];
+
+    const emS = functionSelectors("ExchangeManager");
+    const em = m.contract("ExchangeManager");
+    const emC = [em, FacetCutAction.Replace, Object.values(emS)];
+
+    const evS = functionSelectors("ExchangeView");
+    const ev = m.contract("ExchangeView");
+    const evC = [ev, FacetCutAction.Replace, Object.values(evS)];
+
+    m.call(
+      cutProxy,
+      "diamondCut",
+      [[oC, aC, emC, evC, mC], cpiInit.contract, cpiInit.selector],
+      { id: "IExchangeP2PDiamondCut" }
+    );
+
+    const acMerchant = m.contractAt("Merchant", iExchangeP2P, {
+      id: "IExchangeP2PMerchant",
+    });
+
+    const acOrder = m.contractAt("Order", iExchangeP2P, {
+      id: "IExchangeP2POrder",
+    });
+
+    const acAppeal = m.contractAt("Appeal", iExchangeP2P, {
+      id: "IExchangeP2PAppeal",
+    });
+
+    const acExchangeManager = m.contractAt("ExchangeManager", iExchangeP2P, {
+      id: "IExchangeP2PExchangeManager",
+    });
+
+    const acExchangeView = m.contractAt("ExchangeView", iExchangeP2P, {
+      id: "IExchangeP2PExchangeView",
+    });
+
+    return { acMerchant, acOrder, acAppeal, acExchangeManager, acExchangeView };
+  }
+);
 
 export default IExchangeP2PUpdateModule;
