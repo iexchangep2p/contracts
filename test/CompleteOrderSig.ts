098f3979a864f8a4a7e97c15eb728a325681bdc0
KaePee <obedpksomuah@gmail.com>
2025-02-20 21:45:58 +0000

    Revert Init: payOrder, releaseOrder setup

--- test/CompleteOrderSig.ts
+++ test/CompleteOrderSig.ts
@@ -283,20 +283,7 @@ describe("Complete OrderSig", function () {
       .reverted;
 
     //console.log("Order hash", await viewProxy.order(orderHash));
-    const [
-      trader,
-      merchant,
-      traderChain,
-      merchantChain,
-      token,
-      oCurrency,
-      oPaymentMethod,
-      orderType,
-      orderState,
-      quantity,
-      deadline,
-      createdAt,
-    ] = await viewProxy.order(orderHash);
+    const [, , , , , , , , orderState] = await viewProxy.order(orderHash);
 
     //orderState = pending; 0
     expect(orderState).to.equal(OrderState.pending);
@@ -457,4 +444,108 @@ describe("Complete OrderSig", function () {
 
     console.log("New order status: ", await viewProxy.order(orderHash));
   });
+
+   it("Buy[Reverts For Pay, Release]", async function () {
+     const {
+       kofiMerchant,
+       amaTrader,
+       oneGrand,
+       usdt,
+       currency,
+       paymentMethod,
+       oneGrandNumber,
+       chainId,
+       orderSigProxy,
+       orderProxy,
+       iExchangeP2P,
+     } = await loadFixture(deployIExchange);
+
+     const order = sameChainOrder(
+       amaTrader.address,
+       kofiMerchant.address,
+       await usdt.getAddress(),
+       ethers.keccak256(currency),
+       ethers.keccak256(paymentMethod),
+       oneGrandNumber,
+       OrderType.buy,
+       chainId,
+       chainId
+     );
+
+     const sigchain = orderSigChain(order);
+     const sigchainAddress = await orderSigProxy.getAddress();
+     const domain = iexDomain(sigchain, sigchainAddress);
+
+     const domainHash = iexDomainHash(domain);
+
+     const orderHash = createOrderTypedDataHash(order, domain);
+
+     const sigchainDomainHash = await iExchangeP2P.domainSeparator();
+     expect(domainHash).to.equal(sigchainDomainHash);
+     const traderSig = await signOrder(amaTrader, order, domain);
+     const merchantSig = await signOrder(kofiMerchant, order, domain);
+
+     const traderAddress = ethers.verifyTypedData(
+       domain,
+       encodedCreateOrder().types,
+       order,
+       traderSig
+     );
+     expect(amaTrader.address).to.equal(traderAddress);
+
+     const merchantAddress = ethers.verifyTypedData(
+       domain,
+       encodedCreateOrder().types,
+       order,
+       merchantSig
+     );
+
+     expect(kofiMerchant.address).to.equal(merchantAddress);
+
+     await usdt.connect(kofiMerchant).approve(sigchainAddress, oneGrand);
+
+     await expect(orderSigProxy.createOrder(order, traderSig, merchantSig))
+       .to.emit(usdt, "Transfer")
+       .withArgs(
+         kofiMerchant.address,
+         await orderSigProxy.getAddress(),
+         oneGrand
+       );
+
+     //Reverts for payOrder
+
+     //passing payOrder
+     const payOrderMethod: OrderMethodPayload = makeOrderMethod(
+       orderHash,
+       OrderMethod.pay
+     );
+     const payOrderSig = await signOrderMethod(
+       amaTrader,
+       payOrderMethod,
+       domain
+     );
+
+     await expect(orderSigProxy.payOrder(payOrderMethod, payOrderSig))
+       .to.emit(orderSigProxy, "OrderPaid")
+       .withArgs(orderHash, OrderState.paid);
+
+     //revert for releaseOrder
+
+     //passing releaseOrder
+     const releaseOrderMethod: OrderMethodPayload = makeOrderMethod(
+       orderHash,
+       OrderMethod.release
+     );
+     const releaseOrderSig = await signOrderMethod(
+       kofiMerchant,
+       releaseOrderMethod,
+       domain
+     );
+
+     await expect(
+       orderSigProxy.releaseOrder(releaseOrderMethod, releaseOrderSig)
+     )
+       .to.emit(usdt, "Transfer")
+       .withArgs(await orderSigProxy.getAddress(), amaTrader.address, anyValue);
+   });
 });
