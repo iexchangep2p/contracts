2f62823e90681306e7a71ca6cd50d47972e3ad44
KaePee <obedpksomuah@gmail.com>
2025-02-24 01:49:38 +0000

    passing appeal

---
+++ test/AppealOrderSig.ts
@@ -0,0 +1,123 @@
+import { loadFixture, time } from "@nomicfoundation/hardhat-toolbox/network-helpers";
+import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
+import { ethers, ignition } from "hardhat";
+import { expect } from "chai";
+import { deployIExchange } from "./IExchangeDeployFixture";
+import {
+  createOrderMethodTypedDataHash,
+  createOrderTypedDataHash,
+  encodedCreateOrder,
+  iexDomain,
+  iexDomainHash,
+  makeOrderMethod,
+  OrderMethod,
+  OrderMethodPayload,
+  orderSigChain,
+  OrderState,
+  OrderType,
+  sameChainOrder,
+  signOrder,
+  AppealDecision,
+  signOrderMethod,
+} from "../client";
+
+describe("Appeal Order - OrderSig", function () {
+  it("Should[Appeal]", async function () {
+    const {
+      kofiMerchant,
+      amaTrader,
+      yaaBrokie,
+      oneGrand,
+      usdt,
+      currency,
+      paymentMethod,
+      oneGrandNumber,
+      chainId,
+      orderSigProxy,
+      orderProxy,
+      iExchangeP2P,
+    } = await loadFixture(deployIExchange);
+
+    //createOrder → acceptOrder → payOrder → appealOrder
+
+    const order = sameChainOrder(
+      amaTrader.address,
+      kofiMerchant.address,
+      await usdt.getAddress(),
+      ethers.keccak256(currency),
+      ethers.keccak256(paymentMethod),
+      oneGrandNumber,
+      OrderType.buy,
+      chainId,
+      chainId
+    );
+
+    const sigchain = orderSigChain(order);
+    const sigchainAddress = await orderSigProxy.getAddress();
+    const domain = iexDomain(sigchain, sigchainAddress);
+
+    const domainHash = iexDomainHash(domain);
+
+    const orderHash = createOrderTypedDataHash(order, domain);
+
+    const sigchainDomainHash = await iExchangeP2P.domainSeparator();
+    expect(domainHash).to.equal(sigchainDomainHash);
+    const traderSig = await signOrder(amaTrader, order, domain);
+    const merchantSig = await signOrder(kofiMerchant, order, domain);
+
+    const traderAddress = ethers.verifyTypedData(
+      domain,
+      encodedCreateOrder().types,
+      order,
+      traderSig
+    );
+    expect(amaTrader.address).to.equal(traderAddress);
+
+    const merchantAddress = ethers.verifyTypedData(
+      domain,
+      encodedCreateOrder().types,
+      order,
+      merchantSig
+    );
+
+    expect(kofiMerchant.address).to.equal(merchantAddress);
+
+    await usdt.connect(kofiMerchant).approve(sigchainAddress, oneGrand);
+
+    await expect(orderSigProxy.createOrder(order, traderSig, merchantSig))
+      .to.emit(usdt, "Transfer")
+      .withArgs(
+        kofiMerchant.address,
+        await orderSigProxy.getAddress(),
+        oneGrand
+      );
+
+    const payOrderMethod: OrderMethodPayload = makeOrderMethod(
+      orderHash,
+      OrderMethod.pay
+    );
+    const payOrderSig = await signOrderMethod(
+      amaTrader,
+      payOrderMethod,
+      domain
+    );
+
+    await expect(orderSigProxy.payOrder(payOrderMethod, payOrderSig))
+      .to.emit(orderSigProxy, "OrderPaid")
+      .withArgs(orderHash, OrderState.paid);
+
+    const appealOrderMethod: OrderMethodPayload = makeOrderMethod(
+      orderHash,
+      OrderMethod.pay
+    );
+    const appealOrderSig = await signOrderMethod(
+      kofiMerchant,
+      appealOrderMethod,
+      domain
+    );
+    await expect(orderSigProxy.appealOrder(appealOrderMethod, appealOrderSig))
+      .to.emit(orderSigProxy, "OrderAppealed")
+      .withArgs(orderHash, kofiMerchant.address, AppealDecision.unvoted, OrderState.appealed, anyValue);
+      
+  });
+});
