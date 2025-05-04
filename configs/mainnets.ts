e695fe5ed4c929ddd08d460bef6c03529d81f64c
baahkusi <nanakbaahkusi@gmail.com>
2025-05-04 04:10:47 +0300

    update

--- configs/mainnets.ts
+++ configs/mainnets.ts
@@ -1,5 +1,7 @@
+import 'dotenv/config';
 import { HardhatUserConfig, vars } from "hardhat/config";
 const DEPLOY_KEY_MAIN = vars.get("DEPLOY_KEY_MAIN");
+const BSC_ETHERSCAN_KEY = process.env.BSC_ETHERSCAN_KEY;
 export const mainNetworks: any = {
   base: {
     url: "https://mainnet.base.org",
@@ -7,6 +9,14 @@ export const mainNetworks: any = {
   },
   arbitrum: {
     url: "https://arb1.arbitrum.io/rpc",
+    accounts: [DEPLOY_KEY_MAIN],
+  },
+  celo: {
+    url: "https://forno.celo.org",
+    accounts: [DEPLOY_KEY_MAIN],
+  },
+  bsc: {
+    url: "https://bsc-dataseed.binance.org",
     accounts: [DEPLOY_KEY_MAIN],
   }
 };
@@ -27,10 +37,28 @@ export const mainChains: any[] = [
       apiURL: "https://base.blockscout.com/api",
       browserURL: "https://base.blockscout.com",
     },
+  },
+  {
+    network: "celo",
+    chainId: 42220,
+    urls: {
+      apiURL: "https://celo.blockscout.com/api?apikey=ad32f574-5fa0-4b67-8a54-79be0f54010c",
+      browserURL: "https://celo.blockscout.com",
+    },
+  },
+  {
+    network: "bsc",
+    chainId: 56,
+    urls: {
+      apiURL: "https://api.bscscan.com/api",
+      browserURL: "https://bscscan.com",
+    },
   }
 ];
 
 export const mainKeys = {
   base: "anything",
   arbitrum: "anything",
+  celo: "anything",
+  bsc: BSC_ETHERSCAN_KEY
 };
--- scripts/manager.ts
+++ scripts/manager.ts
@@ -2,7 +2,6 @@ import "dotenv/config";
 import { ethers } from "hardhat";
 
 async function main() {
-
   const p2pContracts: { [key: number]: string } = {
     421614: "0x39a7f0a342a0509C1aC248F379ba283e99c36Ae5",
     2810: "0x87c6346E3074373C41B9654875A6bA8716e8C2B9",
@@ -12,6 +11,10 @@ async function main() {
     44787: "0x0089326cF33fF85f9AA39e02F4557B454327A17F",
     1301: "0x3d63fEc287aD7963B614eD873690A745E635D5Fa",
     296: "0x392E10c23E6000910f7785a076FA7B8BC41F315D",
+    56: "0x92ba0d7Ae56c902A574117712E12ef1E94BFedeC",
+  };
+  const crossChain: { [key: number]: string } = {
+    56: "0x9aE7e92505d98b39a082531bE86C69DAC4F0fEF6",
   };
   const chain = Number((await ethers.provider.getNetwork()).chainId);
 
@@ -20,9 +23,12 @@ async function main() {
     p2pContracts[chain]
   );
 
-  let tx = await p2pManager.setMaxConcurrentOrders(BigInt(1e18))
-  console.log(`p2p set max concurrent orders ${tx.hash} ...`);
-
+  let tx = await p2pManager.addTradeToken(
+    "0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d",
+    crossChain[chain],
+    BigInt(50)
+  );
+  console.log(`set trade token ${tx.hash} ...`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
